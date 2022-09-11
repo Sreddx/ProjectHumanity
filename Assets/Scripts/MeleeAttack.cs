0237e4257e00da77437a3e8c723956b9e8d676f2
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-09-11 16:49:55 -0500

    Early Knockback done

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -5,35 +5,63 @@ using UnityEngine;
 public class MeleeAttack : MonoBehaviour
 
 {
+    public float range = 100f;
+    public float Knockback = 250;
+    public GameObject orientation;
+    public GameObject Enemy;
     Collider weapon;
     Transform weaponTransform; 
+
+
     // Start is called before the first frame update
     void Start()
     {
+        /*
         weapon = GetComponent<Collider>();
         weaponTransform = gameObject.transform;
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1")){
-            weapon.enabled = true;
-            //Rotate weapon and make it bigger
+            /*weapon.enabled = true;
+            Rotate weapon and make it bigger
             weaponTransform.Rotate(0, 99, 0);
             weaponTransform.localScale = new Vector3(0.50f, 1f, 0.50f);
+            */
+            ShootRayCast();
         }
         
         else if (Input.GetButtonUp("Fire1")){
-            weapon.enabled = false;
-            //Return weapon to default values
+            /*weapon.enabled = false;
+            Return weapon to default values
             weaponTransform.Rotate(0, 55, 0);
             weaponTransform.localScale = new Vector3(0.10f, 0.50f, 0.23f);
+            */
         }
         
     }
 
+    void ShootRayCast()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, orientation.transform.forward, out hit, range))
+        {
+            Enemy target = hit.transform.GetComponent<Enemy>();
+
+            if(target != null)
+            {
+                Punch();
+                Debug.Log("Hit");
+            }
+
+        }
+    }
+
     private void Punch(){
+        Enemy.transform.position += transform.forward * Time.deltaTime * Knockback;
 
     }
 }
