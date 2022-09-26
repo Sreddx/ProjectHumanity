6969baa84e879e0c818735968a8e7173cb76e363
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2022-09-26 14:44:55 -0500

    Added enemy damage when colliding

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -14,14 +14,26 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         //If I press f, make the player take damage
-        if(Input.GetKeyDown(KeyCode.F)){
-            playerTakeDamage(50);
-            Debug.Log(GameManager.gameManager._playerHealth.Health);
-        }
+        // if(Input.GetKeyDown(KeyCode.F)){
+        //     playerTakeDamage(50);
+        //     Debug.Log(GameManager.gameManager._playerHealth.Health);
+        // }
         if(Input.GetKeyDown(KeyCode.H)){
             playerHeal(30);
             Debug.Log(GameManager.gameManager._playerHealth.Health);
         }
+    }
+    
+    
+
+
+    private void OnTriggerEnter(Collider other){
+        
+        if(other.tag == "Enemy"){
+            playerTakeDamage(10);
+            Debug.Log(GameManager.gameManager._playerHealth.Health);
+        }
+
     }
 
     private void playerTakeDamage(int damage) {
