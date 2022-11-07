c7fd5fee76fd54e74cc99abbe771b86bf070c86d
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2022-11-07 11:19:45 -0600

    Fixed Enemy AI and made it so it changes player check on chase

--- Assets/Scripts/Enemy/EnemyAi/ChaseState.cs
+++ Assets/Scripts/Enemy/EnemyAi/ChaseState.cs
@@ -7,21 +7,23 @@ public class ChaseState : StateMachineBehaviour
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       EnemyMaster enemyMaster = animator.GetComponent<EnemyMaster>();
-       enemyMaster.ChasePlayer();
-       animator.SetBool("chasingPlayer", true);
+      EnemyMaster enemyMaster = animator.GetComponent<EnemyMaster>();
+      enemyMaster.StartChasingPlayer();
+       
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+      EnemyMaster enemyMaster = animator.GetComponent<EnemyMaster>();
+      enemyMaster.PlayerChaseCheck(); 
     }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       animator.SetBool("chasingPlayer", true);
+      EnemyMaster enemyMaster = animator.GetComponent<EnemyMaster>();
+      enemyMaster.GoBackToPatrol();
     }
 
     
