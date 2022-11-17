824fa42d70a1d4d54c2c042f49f06d581d3bc13f
Sebastian Rojas Salazar <RSsebas08@outlook.com>
2022-11-17 13:28:33 -0600

    Make cam get player reference from script

--- Assets/Scripts/Player/ThirdPersonCam.cs
+++ Assets/Scripts/Player/ThirdPersonCam.cs
@@ -12,6 +12,17 @@ public class ThirdPersonCam : MonoBehaviour
 
     [SerializeField] private float _rotationSpeed;
 
+    private void Awake() {
+        //Get Player references
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        //get child of _player that is the player object
+        _playerObj = _player.GetChild(0);
+        //get child of _player that is orientation
+        _orientation = _player.GetChild(1);
+        rb = _player.GetComponent<Rigidbody>();
+
+    }
+
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -19,7 +30,7 @@ public class ThirdPersonCam : MonoBehaviour
 
     private void Update() 
     {
-        // rotate _orientation
+        // rotate _orientation by checking view direction of camera and rotating orientation of player towards that direction
         Vector3 viewDir = _player.position - new Vector3(transform.position.x, _player.position.y, transform.position.z);
         _orientation.forward = viewDir.normalized;
 
