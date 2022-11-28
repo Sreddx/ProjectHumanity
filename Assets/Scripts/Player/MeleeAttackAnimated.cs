60c4b0c554eadb6271e0897f93ebfeccbd305485
Abraham Mendoza <71141653+Abraham935@users.noreply.github.com>
2022-11-28 12:41:48 -0600

    Update MeleeAttackAnimated.cs

--- Assets/Scripts/Player/MeleeAttackAnimated.cs
+++ Assets/Scripts/Player/MeleeAttackAnimated.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class MeleeAttackAnimated : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0f;
+    float maxComboDelay = 1f;
+    
     void Start()
     {
+        anim = GetComponent<Animator>();
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+        {
+            anim.SetBool("Hit2", false);
+        }
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit3"))
+        {
+            anim.SetBool("Hit3", false);
+        }
+
+        if(Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
+        }
+
+        if(Time.time > nextFireTime)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                OnClick();
+            }
+        }
+    }
+
+
+    void OnClick()
+    {
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1)
+        {
+            anim.SetBool("Hit1", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+
+        if(noOfClicks >=2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit1"))
+        {
+            anim.SetBool("Hit1", false);
+            anim.SetBool("Hit2", true);
+        }
+        if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("Hit2"))
+        {
+            anim.SetBool("Hit2", false);
+            anim.SetBool("Hit3", true);
+        }
+
     }
 }
